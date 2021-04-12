f9cdbf9918369c7012485f08bd0c4d2e4b8a9469
Edgar Benzor <edgarbenz@hotmail.com>
2021-04-11 23:30:03 -0500

    Pasar una funcion como parametro de otra funcion

---
+++ funcionesComoParametrosDeOtraFuncion.js
@@ -0,0 +1,35 @@
+
+function irAlGym (cuandoTerminesLeSiguesConEsto) {
+    console.log("estoy en el GYM entrenado muy duro");
+    cuandoTerminesLeSiguesConEsto();
+}
+
+function comprarEnSoriana () {
+    console.log ("estoy comprando en Soriana");
+}
+
+function pagarTarjetaEnAlsuper {
+    console.log("estoy pagando la tarjeta ",tarjeta," en AlSuper");
+}
+
+irAlGym(pagarTarjetaEnAlsuper);
+
+/* THIS IR ANOTHER EXAMPLE */
+/*
+console.log("\n\n\n****** INICIA OTRO EJEMPLO ********\n\n\n")
+
+function irAlGym2 (nombre,cuandoTerminesLeSiguesConEsto2) {
+    console.log(nombre," esta en el GYM entrenado muy duro");
+    cuandoTerminesLeSiguesConEsto2();
+}
+
+function comprarEnSoriana2 (){
+    console.log ("estoy comprando en Soriana");
+}
+
+function pagarTarjetaEnAlsuper2 () {
+    console.log("estoy pagando la tarjeta en AlSuper");
+}
+
+irAlGym2("Edgar",comprarEnSoriana2);
+*/
